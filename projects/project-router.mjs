91f2e136c38b49aebc23c16e145f4a76bcda5229
DeAndre Johnson <dre@phreshr.com>
2019-12-14 02:56:56 +0800

    add tasks routes

--- projects/project-router.mjs
+++ projects/project-router.mjs
@@ -1,18 +1,19 @@
 import express from "express";
 import Projects from "./projects-model.mjs";
+import Tasks from "../tasks/tasks-model.mjs";
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
     const projects = await Projects.findProjects();
-    const transformedProject = projects.map(project => {
+    const transformedProjects = projects.map(project => {
       return {
         ...project,
         completed: project.completed === 0 ? false : true
       };
     });
-    res.status(200).json(transformedProject);
+    res.status(200).json(transformedProjects);
   } catch (error) {
     res.status(500).json({ error: "Projects could not be retrieved." });
   }
@@ -46,6 +47,43 @@ router.post("/", async (req, res) => {
     res.status(201).json(project);
   } catch (error) {
     res.status(500).json({ error: "Failed to create new project" });
+  }
+});
+
+// Tasks
+router.get("/:id/tasks", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const tasks = await Tasks.findTasks(id);
+    const transformedTasks = tasks.map(task => {
+      return {
+        ...task,
+        completed: task.completed === 0 ? false : true
+      };
+    });
+    res.status(200).json(transformedTasks);
+  } catch (error) {
+    res.status(500).json({ error: "Task not found" });
+  }
+});
+
+router.post("/:id/tasks", async (req, res) => {
+  const { id } = req.params;
+  const task = req.body;
+
+  try {
+    const projects = await Projects.findById(id);
+    if (!projects) {
+      return res.status(404).json({ message: "Invalid Project ID" });
+    }
+    if (!task.description) {
+      return res.status(404).json({ message: "Task description missing" });
+    }
+    const count = await Tasks.add(task, id);
+    res.status(201).json(count);
+  } catch (error) {
+    res.status(500).json({ error: "Error adding task" });
   }
 });
 
