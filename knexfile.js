ac465baef30e9cfc1c846c9562df099148ccf670
DeAndre Johnson <dre@phreshr.com>
2019-12-14 01:01:49 +0800

    fix format of knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,12 +6,12 @@ module.exports = {
     client: "sqlite3",
     connection: {
       filename: "./data/dev.sqlite3"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
-  },
-  migrations: {
-    directory: "./data/migrations"
-  },
-  seeds: {
-    directory: "./data/seeds"
   }
 };
