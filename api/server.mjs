ede6bf71ce935001c8c0bb9032e940b13695a319
DeAndre Johnson <dre@phreshr.com>
2019-12-14 02:38:37 +0800

    add router api endpoint

--- api/server.mjs
+++ api/server.mjs
@@ -1,6 +1,11 @@
 import express from "express";
+import projectRouter from "../projects/project-router.mjs";
 
 const server = express();
+
+server.use(express.json());
+
+server.use("/api/projects", projectRouter);
 
 server.get("/", (req, res) => {
   res.send("<h1>Node DB Sprint</h1>");
